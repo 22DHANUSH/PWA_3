4201310c88f97e951dec8735040f95dff1833be5
Vashisth, Kaushal <Kaushal.Vashisth@columbia.com>
2025-08-28 11:29:34 +0530

    starter template added (#2)

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Counter from './features/counter/Counter';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/counter' element={<Counter />} />
+      </Routes>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
