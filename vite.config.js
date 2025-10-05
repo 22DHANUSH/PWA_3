f1788f79c70e3b3bc2e4e25001546098e622f4ed
Dhanush <dhanushdgowda22@gmail.com>
2025-10-05 13:25:59 +0530

    PWA code added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['redux-persist'],
-  },
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB
+      },
+      manifest: {
+        name: "My E-Commerce App",
+        short_name: "EcomApp",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "columbialogo.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "columbialogo.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "csc_logo.webp",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ['redux-persist'],
+//   },
+// })
